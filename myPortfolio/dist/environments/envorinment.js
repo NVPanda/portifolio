e6d8b65276b2d67bd2f29dc756d19b644ca212a6
Natã Vitor Vaguel Reis <59771238+NVPanda@users.noreply.github.com>
2024-10-17 22:03:18 +0000

    env

---
+++ myPortfolio/dist/environments/envorinment.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environment = void 0;
+exports.environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "SUA_API_KEY",
+        authDomain: "SEU_AUTH_DOMAIN",
+        projectId: "SEU_PROJECT_ID",
+        storageBucket: "SEU_STORAGE_BUCKET",
+        messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+        appId: "SEU_APP_ID"
+    }
+};
